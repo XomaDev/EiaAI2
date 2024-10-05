40f5c0f185502e27c814ea9a2538e4415119235d
kumaraswamy <care.xoma@gmail.com>
2024-10-05 22:33:24 +0530

    map components to eia

--- src/main/kotlin/space/themelon/eia64/Initialize.kt
+++ src/main/kotlin/space/themelon/eia64/Initialize.kt
@@ -4,6 +4,7 @@ package space.themelon.eia64
 
 import com.google.appinventor.components.runtime.Component
 import com.google.appinventor.components.runtime.Form
+import gnu.mapping.Environment
 import space.themelon.eia64.runtime.Executor
 import java.io.ByteArrayOutputStream
 import java.io.File
@@ -13,11 +14,12 @@ import java.util.zip.ZipInputStream
 
 class Initialize(extension: Any) {
 
+    private val form = Form.getActiveForm()
+
     private val stdout = ByteArrayOutputStream()
     private val executor: Executor
 
     init {
-        val form = Form.getActiveForm()
         val stdlib = form.openAssetForExtension(extension as Component, "stdlib.zip")
         val destFolder = File(form.filesDir, "stdlib/")
 
@@ -36,6 +38,36 @@ class Initialize(extension: Any) {
 
         executor = Executor()
         executor.standardOutput = PrintStream(stdout)
+
+        val components = if (form.isRepl) mapComponentsRepl() else mapComponents()
+        components.forEach {
+            executor.defineJavaObject(it.key, it.value)
+        }
+        executor.defineJavaObject("form", form)
+    }
+
+    private fun mapComponents(): Map<String, Component> {
+        val components = HashMap<String, Component>()
+        form.javaClass.fields.forEach {
+            if (it.type == Component::class.java) {
+                components[it.name] = it.get(form) as Component
+            }
+        }
+        return components
+    }
+
+    private fun mapComponentsRepl(): Map<String, Component> {
+        val components = HashMap<String, Component>()
+        // lookup all the components in form environment
+        val environment = form.javaClass.getField("form\$Mnenvironment").get(form) as Environment
+        environment.enumerateAllLocations().forEach { location ->
+            location.value.let {
+                if (it is Component) {
+                    components[location.keySymbol.name] = it
+                }
+            }
+        }
+        return components
     }
 
     fun execute(source: String): Array<Any> {
